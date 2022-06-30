8c62a60855b64a117dc37ab21ed5bdf6ceef0822
Milan Poudel <milanpoudel.safalza@gmail.com>
2022-06-30 12:21:41 +0545

    Create dev branch

--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("hey you are welcome guys");
+  res.send("hey you are welcome guys into development");
 });
 
 module.exports = router;
