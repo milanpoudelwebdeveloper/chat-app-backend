c9556e9937b01abe29afb4c91b455714b6338a0c
milan poudel <milanwebdeveloper1@gmail.com>
2022-07-02 11:35:37 +0545

    create endpoint for uploading image to the cloudinary

---
+++ controllers/cloudinary.js
@@ -0,0 +1,26 @@
+const cloudinary = require("cloudinary").v2;
+
+//config
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+exports.uploadImage = async (req, res) => {
+  const { image } = req.body;
+  //we are going to send the image in the request body
+  try {
+    let result = await cloudinary.uploader.upload(image, {
+      public_id: `${Date.now()}`,
+      resource_type: "auto", //jpeg/png
+    });
+    res.json({
+      url: result.url.toString(),
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Something went wrong while uploading image");
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { uploadImage } = require("../controllers/cloudinary");
 const { registerUser, loginUser } = require("../controllers/user");
 
 const router = express.Router();
@@ -6,5 +7,7 @@ const router = express.Router();
 router.post("/login", loginUser);
 
 router.post("/register", registerUser);
+//this one will be for uploading profile picture or any image
+router.post("/uploadImage", uploadImage);
 
 module.exports = router;
